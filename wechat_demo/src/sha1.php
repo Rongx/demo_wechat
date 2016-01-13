a6267d852702606b594c3ee96d7f26417819fd21
rongx <lin20090425@yeah.net>
2016-01-14 00:10:39 +0800

    更新SHA1算法基础类

--- wechat_demo/src/sha1.php
+++ wechat_demo/src/sha1.php
@@ -1,14 +1,12 @@
 <?php
+/**
+ * SHA1 class
+ *
+ * SHA1算法基础类
+ */
+include_once "errorCode.php";  
 
-  include_once "errorCode.php";  
-
-  /**
-   * SHA1 class
-   *
-   * 
-   */
-  class SHA1
-  {
+class SHA1{
     /**
      * 用SHA1算法生成安全签名
      * @param string $token 票据
@@ -16,8 +14,7 @@
      * @param string $nonce 随机字符串
      * @param string $encrypt 密文消息
      */
-    public function getSHA1($token, $timestamp, $nonce, $encrypt_msg)
-    {
+    public function getSHA1($token, $timestamp, $nonce, $encrypt_msg){
     	//排序
     	try {
         $array = array($encrypt_msg, $token, $timestamp, $nonce);
@@ -30,6 +27,6 @@
     	}
     }  
 
-  }
-
+}
+?>
 
